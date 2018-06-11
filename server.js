c9f5587c529f1d3f9865daf7f962fcb48270a769
unknown <zachsotak@yahoo.com>
2018-06-11 22:45:03 +0100

    finish server api

--- server.js
+++ server.js
@@ -1,11 +1,111 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors')
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}))
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id:'987',
+            has: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+
+app.post('/signin', (req, res) =>{
+    if(req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password){
+        res.json(database.users[0]);
+    }else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+})
 
 
 
+// Load hash from your password DB.
 
 
-app.listen(3000);
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
+
+/*
+* --> res = this is working, this is the root route
+* /signin route will be a --> POST request, becuase we are sending a password and respond with success or fail
+* /register --> POST cuz we want to add the data to a database with new user info, return new created user, POST = user
+* /In home screen give profile ability to access profile of the user from the home screen --> GET = user
+* /image --> PUT because user exists and we want to update, return user
+*
+*
+* */
